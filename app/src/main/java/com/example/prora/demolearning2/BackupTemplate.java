c60a2e0c0308e12d929a0dd0b6814edbac2e0d62
anhdn <anh.dn@rubycell.com>
2016-10-17 17:19:08 +0700

    request permission

---
+++ app/src/main/java/com/example/prora/demolearning2/BackupTemplate.java
@@ -0,0 +1,18 @@
+package com.example.prora.demolearning2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+/**
+ * Created by RUBYCELL on 10/17/2016.
+ */
+
+public abstract class BackupTemplate {
+
+
+}
