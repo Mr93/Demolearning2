e80a885d131cecc09cd6a5db8239c12dea024972
datnd <dat.nd@rubycell.com>
2016-10-18 09:29:50 +0700

    Merge remote-tracking branch 'remotes/origin/feature/tree_app' into develop
    Conflicts:
    	app/src/main/java/com/example/prora/demolearning2/MainActivity.java

--- app/src/main/java/com/example/prora/demolearning2/MainActivity.java
+++ app/src/main/java/com/example/prora/demolearning2/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,8 +17,16 @@ import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
+import android.view.View;
+import com.example.prora.demolearning2.strategy.ContactStrategy;
+import com.example.prora.demolearning2.strategy.IStrategy;
+import com.example.prora.demolearning2.strategy.SmsStrategy;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+	IStrategy strategy;
 
 	private final int PERMISSION_REQUEST = 1;
 
@@ -26,6 +35,29 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		getRuntimePermission();
+		findViewById(R.id.btnContact).setOnClickListener(this);
+		findViewById(R.id.btnSms).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		//Doan nay se get ra concreate Strategy sau
+		int id = view.getId();
+		Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+		switch (id){
+			case R.id.btnContact:
+				strategy = new ContactStrategy();
+				intent.putExtra(getResources().getString(R.string.strategy), (Serializable) strategy);
+				startActivity(intent);
+				break;
+			case R.id.btnSms:
+				strategy = new SmsStrategy();
+				intent.putExtra(getResources().getString(R.string.strategy), (Serializable) strategy);
+				startActivity(intent);
+				break;
+			default:
+				break;
+		}
 	}
 
 	@TargetApi(23)
